d1ee63b5eae440b1e50758e3b4d5a0e66222b6a9
August Kliment <augustkliment94@gmail.com>
2025-11-08 16:08:30 -0600

    Had some trouble

--- factorial.js
+++ factorial.js
@@ -1,5 +1,11 @@
 function factorial(num) {
-  return 0;
+  if (num < 0)
+    return 0;
+  else if (num == 0)
+    return 1;
+    else {
+      return (num * factorial(num -1));
+  }
 }
 
 module.exports = factorial;
