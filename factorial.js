baf7e18dc59f38266cdec523471087feee8097bf
August Kliment <augustkliment94@gmail.com>
2025-11-08 16:44:33 -0600

    redux

--- factorial.js
+++ factorial.js
@@ -1,11 +1,22 @@
-function factorial(num) {
-  if (num < 0)
-    return 0;
-  else if (num == 0)
-    return 1;
-    else {
-      return (num * factorial(num -1));
+function factorial(num) 
+{
+  let result = 1;
+
+  for (let i = num; i > 1; i--)
+  {
+   
+    result *= i; 
   }
+
+  return result;
 }
+
+//     return 0;
+//   else if (num == 0)
+//     return 1;
+//     else {
+//       return (num * factorial(num -1));
+//   }
+// }
 
 module.exports = factorial;
